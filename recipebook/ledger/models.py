eece6b89da23cada1e3ea0522ca1050fbd9a15da
LegitAgent125 <albamartindarius@gmail.com>
2025-02-27 23:01:48 +0800

    fixed server and syntax errors

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,11 +1,36 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
-class Ingredient():
-    return
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
     
-class Recipe():
-    return
+    def __str__(self):
+        return '{}: Ingredient'.format(self.name)
     
-class RecipeIngredient():
-    return
+    def get_absolute_url(self):
+        return reverse('ingredient', args=[self.pk])
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return '{}: Recipe'.format(self.name)
+    
+    def get_absolute_url(self):
+        return reverse('ledger:recipe/detail', args=[self.pk])
+    
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=255)
+    
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name="recipe"
+    )
+    
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name="ingredients"
+    )
+    
