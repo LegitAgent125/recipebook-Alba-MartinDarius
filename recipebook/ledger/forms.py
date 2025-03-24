b505ca031b875b9b9ca69778e6d398996343239f
Alba Martin <albamartindarius@gmail.com>
2025-03-24 23:00:21 +0800

    Fixed urls.py and added comment for forms.py

--- recipebook/ledger/forms.py
+++ recipebook/ledger/forms.py
@@ -9,4 +9,4 @@ class RecipeForm(forms.ModelForm):
 class RecipeImageForm(forms.ModelForm):
     class Meta:
         model = RecipeImage
-        exclude = ['recipe']
+        exclude = ['recipe'] # from https://testdriven.io/tips/ca45db78-da38-4ba0-884d-9be0bc1e3fbf/
--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name="recipes/list"),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name="recipe/detail"),
     path('recipe/add', add_recipe, name='recipe/add'),
-    path('recipes/<int:pk>/uploadimage', upload_recipe_img, name='upload_recipe_img'),
+    path('recipes/<int:pk>/add_image', upload_recipe_img, name='upload_recipe_img'),
 ]
 
 app_name = "ledger"
